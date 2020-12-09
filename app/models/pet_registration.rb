6eca45177dbaa4caddb5701b0e685f60ad0992ef
HeitorBraga <heitorbrasil62@gmail.com>
2020-12-09 20:26:21 -0300

    fix constant place

--- app/models/pet_registration.rb
+++ app/models/pet_registration.rb
@@ -1,10 +1,10 @@
 class PetRegistration < ApplicationRecord
+  CHARS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  
   belongs_to :person
   has_one_attached :photografy
 
   validates :name, :kind, :breed, :birthdate, :photografy, :person, presence: true
-
-  CHARS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
   before_save do
     if code.blank?
