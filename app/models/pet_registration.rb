dbc4ea7ae15388eb965d66f5a28248a90b81fb31
HeitorBraga <heitorbrasil62@gmail.com>
2020-12-09 20:18:45 -0300

    Adicionei a forma para geração do Código

--- app/models/pet_registration.rb
+++ app/models/pet_registration.rb
@@ -3,11 +3,16 @@ class PetRegistration < ApplicationRecord
   has_one_attached :photografy
 
   validates :name, :kind, :breed, :birthdate, :photografy, :person, presence: true
-end
 
-PetRegistration.create(name: nil).valid?
-PetRegistration.create(kind: nil).valid?
-PetRegistration.create(breed: nil).valid?
-PetRegistration.create(birthdate: nil).valid?
-PetRegistration.create(photografy: nil).valid?
-PetRegistration.create(person: nil).valid?
+  CHARS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  before_save do
+    if code.blank?
+      begin
+        _code = (0...6).collect { CHARS[Kernel.rand(CHARS.length)] }.join
+      end while PetRegistration.find_by_code(_code)
+
+      self.code = _code.gsub(/(\w{3})(\w{3})/, '\1-\2')
+    end
+  end
+end
