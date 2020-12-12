d893541225555c26c3fe43e75d72f001c66c84c2
HeitorBraga <heitorbrasil62@gmail.com>
2020-12-12 14:14:26 -0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  devise_for :people
-  resources :people
+  devise_for :people, controllers: { registrations: "people" }
+  resources :people, only: [:index, :edit, :updated]
   resources :pet_registrations
 
-  get '/perfil' => 'people#profile', as: :person_profile
+  get "/perfil" => "people#profile", as: :person_profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
